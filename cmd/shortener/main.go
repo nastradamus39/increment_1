0afea6cef62dedd3a6913d21a98192a1263acd7d
nastradamus39 <a.v.krizhanovsky@k50dev.ru>
2022-04-25 19:36:35 +0300

    Сервер доступен по адресу: http://localhost:8080

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import "net/http"
+
+// HelloWorld — обработчик запроса.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+}
+
+func main() {
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8080", nil)
+}
