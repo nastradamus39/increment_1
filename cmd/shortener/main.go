a3c7a7cd2d275343515e8a082ae9a67830af9e06
nastradamus39 <a.v.krizhanovsky@k50dev.ru>
2022-04-25 22:39:38 +0300

    v1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,13 +1,83 @@
 package main
 
-import "net/http"
+import (
+	"crypto/sha256"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
 
-// HelloWorld — обработчик запроса.
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello, World</h1>"))
+var urls = map[string]string{}
+
+// PostUrl — создает короткий урл.
+func PostUrl(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("POST метод")
+
+	body, _ := ioutil.ReadAll(r.Body)
+	url := string(body)
+
+	defer r.Body.Close()
+
+	h := sha256.New()
+	h.Write(body)
+
+	key := fmt.Sprintf("%x", h.Sum(nil))
+
+	urls[key] = url
+
+	fmt.Println(urls)
+
+	w.WriteHeader(http.StatusCreated)
+
+	w.Write([]byte(key))
+}
+
+// GetShortUrl — возвращает полный урл по короткому.
+func GetShortUrl(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET метод")
+
+	id := strings.Trim(r.RequestURI, "/")
+
+	url := urls[id]
+
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	w.Header().Add("Location", url)
+
+	w.Write([]byte(url))
+}
+
+func Short(w http.ResponseWriter, r *http.Request) {
+
+	// в зависимости от метода
+	switch r.Method {
+	// если методом POST
+	case "POST":
+		PostUrl(w, r)
+	// если методом GET
+	case "GET":
+		GetShortUrl(w, r)
+	default:
+		fmt.Println("Неизвестный метод")
+	}
+
 }
 
 func main() {
-	http.HandleFunc("/", HelloWorld)
-	http.ListenAndServe(":8080", nil)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", Short)
+
+	server := http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	fmt.Println("Starting server on :8080")
+
+	server.ListenAndServe()
 }
